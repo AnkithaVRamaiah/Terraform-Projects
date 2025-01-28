e247d9c1c1c6294e0d9cc4e69596100cc341d6a9
ankitha <vankitha2211@gmail.com>
2025-01-28 13:24:47 +0530

    second project

--- Project2/main.tf
+++ Project2/main.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   aws_region = "us-east-1"
   ami_value = "ami-0411d0" # replace this
   instance_type_value = "t2.micro"
-  key_name_value      = "xyz" # replace this/
+  key_name_value      = "xyz" # replace this
 
 }
 
